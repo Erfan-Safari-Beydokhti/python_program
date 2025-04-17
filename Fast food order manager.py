7153532d1d3d1aba054308f12035b6e53783a30a
Erfan <erfansafari.b.86@gmail.com>
2025-04-17 18:08:00 +0300

    show basket
    create function for show order basket

--- Fast food order manager.py
+++ Fast food order manager.py
@@ -9,9 +9,21 @@ basket={}
 def show_menu():
     for item in menu.keys():
         print(f"{item}: {menu[item]}")
+
 def add_basket(item):
     if item in menu:
         basket[item]=basket.get(item,0)+1
         print(f"{item}: added to your basket")
     else:
         print("your item does not exist")
+
+def show_basket():
+    if not basket:
+      print("Your basket is empty ")
+    else:
+        total=0
+        for item,count in basket.items():
+            price=menu.get(item,0)*count
+            print(f"{item} *{count} = {price}")
+            total+=price
+        print(f"total : {total}")
