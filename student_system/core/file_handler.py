cfc29af12115c5ce4ba66483618ab5c5275fec65
Erfan <erfansafari.b.86@gmail.com>
2025-04-24 19:58:39 +0300

    Add save_student function into file_handler.py

--- student_system/core/file_handler.py
+++ student_system/core/file_handler.py
@@ -1,1 +1,8 @@
 import csv
+data_path="..\data\students.csv"
+def save_students(student):
+    with open(data_path, "w", newline="") as csvfile:
+       field_name=["ID","Name","Age","Grade"]
+       writer=csv.DictWriter(csvfile, fieldnames=field_name)
+       writer.writeheader()
+       writer.writerow(student)
