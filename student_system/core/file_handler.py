72d60ff20c62ed72fe356d5b34e5ffac4ac51c2c
Erfan <erfansafari.b.86@gmail.com>
2025-04-24 20:29:51 +0300

    Add error handler for save_student function

--- student_system/core/file_handler.py
+++ student_system/core/file_handler.py
@@ -1,17 +1,20 @@
 import csv
 import os
+from logging import exception
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, '..', 'data', 'students.csv')
 
 def save_students(students):
-    os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
-    with open(DATA_PATH, mode='w', newline='') as csvfile:
-        fieldnames = ['ID', 'Name', 'Age', 'Grade']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(students)
-
+    try:
+        os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
+        with open(DATA_PATH, mode='w', newline='') as csvfile:
+            fieldnames = ['ID', 'Name', 'Age', 'Grade']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(students)
+    except Exception as e:
+        print("خطا هنگام ذخیره اطلاعات")
 def load_students():
     if not os.path.exists(DATA_PATH):
         return []
