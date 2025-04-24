2603f8cf2551fda2a420f5a540a4f0b355bfe2a3
Erfan <erfansafari.b.86@gmail.com>
2025-04-24 19:48:44 +0300

    Add choice menu into main.py

--- student_system/main.py
+++ student_system/main.py
@@ -1,1 +1,9 @@
-from core
+from core import add_student , view_students , remove_student
+def main ():
+    while True:
+        print("\n ♦♦♦ Student Management System ♦♦♦ ")
+        print("1. نمایش لیست دانش آموزان ")
+        print("2. افزودن دانش آموز ")
+        print("3. حذف دانش آموز ")
+        print("4. خروج ")
+        choice=input("انتخاب شما: ")
