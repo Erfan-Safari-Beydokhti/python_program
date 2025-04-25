33adae72223e5ad926ad5b1016ff39c32488550c
Erfan <erfansafari.b.86@gmail.com>
2025-04-25 19:05:37 +0300

    Translate all code to English

--- management_File/main.py
+++ management_File/main.py
@@ -26,81 +26,81 @@ def save_customers(customers):
 
 def add_customer():
     customers = load_customers()
-    print("\n افزدون مشتری جدید ")
+    print("\n Add new customer : ")
     id=len(customers)+1
-    name=input("نام مشتری:").strip()
-    phone=input("شماره تلفن مشتری:").strip()
-    email=input("ایمیل مشتری:").strip()
+    name=input("Customer's name : ").strip()
+    phone=input("Customer's phone number :").strip()
+    email=input("Customer's email address : ").strip()
     new_customer=Customer(id,name,phone,email)
     customers.append(new_customer)
 
     save_customers(customers)
-    print("مشتری با موفقیت اضافه شد ")
+    print("Customer added successfully.")
 
 
 def list_customers():
     customers = load_customers()
     if not customers:
-        print("لیستی از مشتری ها وجود ندارد")
+        print("There is no list of customers.")
         return
-    print("لیست مشتری ها ")
+    print("Customers list")
     for c in customers:
         print(f"id:{c.id} , name:{c.name} , phone:{c.phone} , email:{c.email}")
     print()
 
 
 def create_invoice():
-    print("صدور فاکتور جدید")
-    customer_id=input("شماره کاربری مشتری:").strip()
+    print("Issuance of a new invoice")
+    customer_id=input("Customer ID: ").strip()
     customers=load_customers()
     if not any(c.id == int(customer_id)for c in customers):
-        print("مشتری با این شماره کاربری پیدا نشد")
+        print("Customer with this ID was not found.")
         return
     items=[]
     while True:
-        name=input("نام محصول : ( پایان )برای خروج").strip()
-        if name =="پایان":
-            print("پایان صدور فاکتور")
+        name=input("Product Name: (End) for Exit : ").strip()
+        if name.lower() =="end":
+            print("End of invoice issuance")
             break
         try:
-            price=float(input("قیمت محصول را وارد کنید:"))
-            quantity=int(input("تعداد محصول:"))
+            price=float(input("Enter the product price : "))
+            quantity=int(input("Number of products : "))
             items.append({"name":name,"price":price,"quantity":quantity})
-            print(f" با موفقیت اضافه شد{name}")
+            print(f" {name} Added successfully. ")
         except ValueError:
-            print("ورودی نامعتبر")
+            print("Invalid input")
     invoice_id=str(uuid.uuid4())[:8]
     invoice=Invoice(invoice_id,customer_id,items)
     invoice_path=f"data/invoices/invoice_{customer_id}.json"
     with open(invoice_path, "w") as file:
         json.dump(invoice.to_dict(),file,indent=4)
-    print(f"ثبت شد{invoice_id}فاکتور با شناسه ")
+    print(f"Invoice with ID {invoice_id} registered. ")
 
 
 def list_invoices():
-    print("لیست فاکتور ها")
+    print("List of invoices")
     invoice_folder="data/invoices"
     files=os.listdir(invoice_folder)
     if not files:
-        print("هیچ فاکتوری وجود ندارد")
+        print("There is no invoice.")
         return
     for file in files:
         file_path=os.path.join(invoice_folder,file)
         with open(file_path, "r") as file:
             data=json.load(file)
             invoice=Invoice.from_dict(data)
-            print(f" فاکتور {invoice.invoice_id} | مشتری {invoice.customer_id} | تاریخ {invoice.date} | مبلغ کل: {invoice.total_price():,.0f} تومان ")
+            print(f" Invoice {invoice.invoice_id} | Customer {invoice.customer_id} | Date {invoice.date} | Total amount: {invoice.total_price():,.0f} Toman ")
         print()
 
 
 def main():
     while True:
-        print("\n سیستم مدیریت مشتری و فاکتور :")
-        print("1. افزودن مشتری جدید")
-        print("2. مشاهده لیست مشتری ها")
-        print("3. افزودن فاکتور جدید ")
-        print("4. مشاهده فاکتور ها")
-        print("5. خروج")
+        print("\n Customer and Invoice Management System:")
+        print("1. Add new customer")
+        print("2. View customer list")
+        print("3. Add new invoice ")
+        print("4. View invoices")
+        print("5. Exit")
 
         choice = input("Choose an option: ").strip()
         if choice == "1":
